f01f826447fe8e6ba7b5aef700fdacc4ea353f31
evanfuhr <evanfuhr@gmail.com>
2015-03-28 11:46:40 -0500

    add dynamic button creation for landing page

--- app/src/main/java/com/example/evan/mhrassistant/MainActivity.java
+++ app/src/main/java/com/example/evan/mhrassistant/MainActivity.java
@@ -5,6 +5,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -14,6 +20,51 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TableLayout heroTable = (TableLayout) findViewById(R.id.TableLayout_hero_table);
+
+        //Rows will be as wide as the table, but wrap content vertically
+        TableRow.LayoutParams table_row_params = new TableRow.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //Buttons will always be aligned to the right of the table
+        for (int j=0;j<3;j++) {
+            //Create new table row
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(table_row_params);
+
+            //Create hero textView
+            TextView hero_name = new TextView(this);
+            hero_name.setText("Hero "+j);
+            hero_name.setTextAppearance(this, android.R.style.TextAppearance_Material_Medium);
+
+            //Add the hero name to the table row
+            tableRow.addView(hero_name);
+
+            //Create hero button
+            final Button hero_button = new Button(this);
+            hero_button.setText("GO");
+            //Use the hero id from the hero table when that part is built
+            hero_button.setId(j+1);
+            //Set click listener for the button
+            hero_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onClickButtonPlayerSheet(view);
+                }
+            });
+
+            //Add the button to the table row
+            tableRow.addView(hero_button);
+
+            heroTable.addView(tableRow);
+
+        }
+    }
+
+    private void onClickButtonPlayerSheet(View view) {
+        Intent intent = new Intent(this, PlayerSheetActivity.class);
+        startActivity(intent);
     }
 
 
